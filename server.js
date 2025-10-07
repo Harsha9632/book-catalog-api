72378c27dd6d8868dfee07a9b95fc512c4a0398f
Harsha Vardhan <hv374783@gmail.com>
2025-10-07 16:30:05 +0530

    Added root route and environment check to server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+
 require('dotenv').config();
-const express    = require('express');
-const connectDB  = require('./config/db');
+const express = require('express');
+const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 const errorHandler = require('./middleware/errorMiddleware');
-
 
 const app = express();
 
@@ -14,7 +14,10 @@ app.use((req, res, next) => {
   next();
 });
 
+
 app.use(express.json());
+
+
 connectDB();
 
 
@@ -22,10 +25,19 @@ app.use('/api/users', userRoutes);
 app.use('/api/books', bookRoutes);
 
 
+app.get('/', (req, res) => {
+  res.send(' Book Catalog API is running successfully!');
+});
+
+
 app.use(errorHandler);
+
+
+console.log('MONGO_URI present:', Boolean(process.env.MONGO_URI));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
